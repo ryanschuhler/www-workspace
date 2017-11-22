9c42413ad9235f8f54c5e2daf35d39d0b14a0702
ryanschuhler <ryan.schuhler@liferay.com>
2017-11-22 13:49:41 -0800

    Initial Commit

---
+++ modules/my-guestbook-project/src/main/java/com/liferay/docs/guestbook/constants/GuestbookPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.docs.guestbook.constants;
+
+/**
+ * @author ryan
+ */
+public class GuestbookPortletKeys {
+
+	public static final String Guestbook = "Guestbook";
+
+}
---
+++ modules/my-guestbook-project/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="my-guestbook-project.caption"/></b>
+</p>
