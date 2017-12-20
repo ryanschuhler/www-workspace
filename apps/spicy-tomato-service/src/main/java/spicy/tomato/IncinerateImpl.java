b9c92f7e802ebafbc11cb6fd425d57e80b46e825
ryanschuhler <ryan.schuhler@liferay.com>
2017-12-19 17:05:34 -0800

    Partial impl services

--- apps/spicy-tomato-service/src/main/java/spicy/tomato/IncinerateImpl.java
+++ apps/spicy-tomato-service/src/main/java/spicy/tomato/IncinerateImpl.java
@@ -1,5 +1,8 @@
 package spicy.tomato;
 
+import import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
+import import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalService;
+
 import org.osgi.service.component.annotations.Component;
 
 /**
@@ -15,5 +18,74 @@ public class IncinerateImpl implements Incinerate {
 	public void sayHello() {
 		System.out.println("Hello World!");
 	}
-	
+
+	public void addTemplates(String[] templates) {
+		for (String template : templates) {
+			long groupId = ;
+			long resourceClassNameId= ;
+			File script = ;
+			String templateKey = ;
+			long groupId = ;
+			long classNameId = PortalUtil.getClassNameId(
+				DDMStructure.class.getName());
+			long structureId = ;
+
+			DDMTemplate ddmTemplate = DDMTemplateLocalService.fetchTemplate(
+				long groupId, long classNameId, String templateKey);
+
+			if (ddmTemplate != null) {
+				DDMTemplateLocalService.updateTemplate(
+					ddmTemplate.getUserId(), ddmTemplate.getTemplateId(),
+					ddmTemplate.getClassPK(), ddmTemplate.getNameMap,
+					ddmTemplate.getDescriptionMap(), ddmTemplate.getType(),
+					ddmTemplate.getMode(), ddmTemplate.getLanguage(),
+					script, ddmTemplate.getCacheable(),
+					ddmTemplate.getSmallImage(), ddmTemplate.getSmallImageURL(),
+					ddmTemplate.getSmallImageFile(), new ServiceContext())
+			}
+			else {
+				DDMTemplateLocalService.addTemplate(
+					_USER_ID, groupId, classNameId, structureId,
+					resourceClassNameId, templateKey, Collections.emptyMap(),
+					Collections.emptyMap(), StringPool.BLANK, StringPool.BLANK,
+					StringPool.BLANK, script, Boolean.TRUE, Boolean.FALSE,
+					StringPool.BLANK, null, serviceContext)
+			}
+		}
+	}
+
+	public void addStructures(String[] structures) {
+		for (String structure : structures) {
+			long groupId = ;
+			long classNameId = PortalUtil.getClassNameId(
+				JournalArticle.class.getName());
+			String structureKey = ;
+			String definition = ;
+			storageType = "json";
+
+			DDMStructure ddmStructure = DDMStructureLocalService.fetchStructure(
+				groupId, classNameId,
+				DDMStructureConstants.DEFAULT_PARENT_STRUCTURE_ID);
+
+			if (ddmStructure != null) {
+				DDMStructureLocalService.updateStructure(
+					ddmStructure.getGroupId(),
+					ddmStructure.getParentStructureId(),
+					ddmStructure.getClassNameId(),
+					ddmStructure.getStructureKey(), ddmStructure.getNameMap(),
+					ddmStructure.getDescriptionMap(), definition,
+					new ServiceContext())
+			}
+			else {
+				DDMStructureLocalService.addStructure(
+					_USER_ID, groupId,
+					DDMStructureConstants.DEFAULT_PARENT_STRUCTURE_ID,
+					classNameId, structureKey, Collections.emptyMap(),
+					Collections.emptyMap(), null, null, storageType, -1,
+					new ServiceContext())
+			}
+		}
+	}
+
+	private static final _USER_ID = 20156L;
 }
