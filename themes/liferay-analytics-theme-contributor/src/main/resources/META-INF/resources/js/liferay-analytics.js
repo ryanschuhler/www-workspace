819ae01a3df7ab835dad4efd99fce7e279407e36
ryanschuhler <ryan.schuhler@liferay.com>
2018-01-10 12:44:23 -0800

    Remove theme contributors

--- themes/liferay-analytics-theme-contributor/src/main/resources/META-INF/resources/js/liferay-analytics.js
+++
@@ -1,9 +0,0 @@
-(function(u, c, a, m, o,l){o="script",l=document,a=l.createElement(o)
-	,m=l.getElementsByTagName(o)[0],a.async=1,a.src=u,a.onload=c,
-	m.parentNode.insertBefore(a,m)})('https://s3-eu-west-1.amazonaws.com/com-liferay-analytics/analytics-all-min.js', function() {
-
-	Analytics.create({ analyticsKey: 'www-webteam.liferay.com' });
-	Analytics.send('view', 'Layout');
-});
-
-console.log('Liferay analytics code loaded');
