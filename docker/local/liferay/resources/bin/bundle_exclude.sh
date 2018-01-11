78e9d3724f4a0937204202396b1a35a96ab367cf
allen-ziegenfus <allen.ziegenfus@liferay.com>
2018-01-11 12:03:04 -0800

    Get liferay running as liferay user. Simplify bundle exclusion

--- docker/local/liferay/resources/bin/bundle_exclude.sh
+++ docker/local/liferay/resources/bin/bundle_exclude.sh
@@ -2,34 +2,19 @@
 
 OSGI_FOLDER=${LIFERAY_HOME}/osgi/marketplace
 
-zip -d ${OSGI_FOLDER}/Liferay\ Connected\ Services\ Client.lpkg lcs-portlet-7.0.10.38.war
-zip -d ${OSGI_FOLDER}/Liferay\ Foundation.lpkg  com.liferay.portal.security.ldap-2.2.6.jar
-zip -d ${OSGI_FOLDER}/Liferay\ Foundation.lpkg  com.liferay.portal.settings.authentication.ldap.web-1.0.14.jar
-zip -d ${OSGI_FOLDER}/Liferay\ Documentum\ Connector.lpkg  documentum-hook-7.0.10.11.war
-zip -d ${OSGI_FOLDER}/Liferay\ Sharepoint\ Connector.lpkg  sharepoint-hook-7.0.10.4.war
-zip -d ${OSGI_FOLDER}/Liferay\ Marketplace.lpkg com.liferay.marketplace.api-4.2.0.jar
-zip -d ${OSGI_FOLDER}/Liferay\ Marketplace.lpkg com.liferay.marketplace.app.manager.web-1.0.17.jar
-zip -d ${OSGI_FOLDER}/Liferay\ Marketplace.lpkg com.liferay.marketplace.deployer-2.0.9.jar
-zip -d ${OSGI_FOLDER}/Liferay\ Marketplace.lpkg com.liferay.marketplace.service-2.1.28.jar
-zip -d ${OSGI_FOLDER}/Liferay\ Marketplace.lpkg com.liferay.marketplace.store.web-2.0.11.jar
-
-
 remove_module () {
     zip -d "${OSGI_FOLDER}/$1" $2
 }
 
-remove_collaboration () {
-    remove_module Liferay\ Collaboration.lpkg $1
-}
-
-remove_collaboration com.liferay.wiki.api-2.4.3.jar
-remove_collaboration com.liferay.wiki.editor.configuration-1.0.8.jar
-remove_collaboration com.liferay.wiki.engine.creole-2.0.10.jar
-remove_collaboration com.liferay.wiki.engine.html-2.0.4.jar
-remove_collaboration com.liferay.wiki.engine.input.editor.common-2.0.3.jar
-remove_collaboration com.liferay.wiki.engine.lang-1.0.1.jar
-remove_collaboration com.liferay.wiki.engine.text-2.0.2.jar
-remove_collaboration com.liferay.wiki.layout.prototype-1.0.6.jar
-remove_collaboration com.liferay.wiki.navigation.web-2.0.6.jar
-remove_collaboration com.liferay.wiki.service-1.6.7.jar
-remove_collaboration com.liferay.wiki.web-2.1.36.jar
+remove_module Liferay\ Connected\ Services\ Client.lpkg lcs-portlet-7.0.10.38.war
+remove_module Liferay\ Foundation.lpkg  com.liferay.portal.security.ldap-2.2.6.jar
+remove_module Liferay\ Foundation.lpkg  com.liferay.portal.settings.authentication.ldap.web-1.0.14.jar
+remove_module Liferay\ Documentum\ Connector.lpkg  documentum-hook-7.0.10.11.war
+remove_module Liferay\ Sharepoint\ Connector.lpkg  sharepoint-hook-7.0.10.4.war 
+remove_module Liferay\ Marketplace.lpkg com.liferay.marketplace*
+remove_module Liferay\ Sync\ Connector.lpkg com.liferay.sync*
+remove_module Liferay\ Collaboration.lpkg com.liferay.message.boards*
+remove_module Liferay\ Collaboration.lpkg com.liferay.social*
+remove_module Liferay\ Collaboration.lpkg com.liferay.wiki*
+remove_module Liferay\ Forms\ and\ Workflow.lpkg com.liferay.polls*
+remove_module Liferay\ Forms\ and\ Workflow.lpkg com.liferay.calendar*
---
+++ docker/local/liferay/resources/create_bundle_list.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+
+OSGI_FOLDER=${LIFERAY_HOME}/osgi/marketplace
+
+for file in ${OSGI_FOLDER}/*.lpkg; do
+    echo ${file}
+    unzip -l "${file}" | grep -i [jar\|war] | awk -v file="${file}" '{print file ":" $4}' | sed 's/ /\\ /g'  | sed 's/:/ /g'
+done
