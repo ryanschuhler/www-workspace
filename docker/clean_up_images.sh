38de1eec97be7da1a2f7fe787c85a0d7e812793f
allen-ziegenfus <allen.ziegenfus@liferay.com>
2018-01-12 01:30:41 -0800

    Add clean up scripts

--- docker/clean_up_images.sh
+++ docker/clean_up_images.sh
@@ -1,1 +1,10 @@
 docker images -a | grep none | awk '{ print $3} ' | xargs docker rmi 
+
+
+
+#docker rm $(docker ps -a -q)
+#* docker rmi $(docker images -a -q)
+#   * docker images -q --filter "dangling=true" | xargs docker rmi
+#* docker volume rm $(docker volume ls -q)
+#   * docker volume rm $(docker volume ls -qf dangling=true)
+#* rm -rf ~/Library/Containers/com.docker.docker/Data/*
