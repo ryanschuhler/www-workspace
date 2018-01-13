a3649b82642364687a53fd3a1a467e0c68e6def7
allen-ziegenfus <allen.ziegenfus@liferay.com>
2018-01-12 18:29:31 -0800

    Update db

--- docker/local/mariadb/resources/docker-entrypoint-initdb.d/www_lportal-updatedb.sql
+++ docker/local/mariadb/resources/docker-entrypoint-initdb.d/www_lportal-updatedb.sql
@@ -1,5 +1,6 @@
 /* Map invalid user ids in WCM */
 
-update DDMStructure set userid = 10159 where userid = 10729;
-update DDMTemplate set userid = 10159 where userid = 10729;
-update DDMStructureVersion set userid = 10159 where userid = 10729;
+update DDMStructure set userid = 10159 where userid in (10729, 154427);
+update DDMStructureVersion set userid = 10159 where userid in (10729, 154427); 
+update DDMTemplate set userid = 10159 where userid in (10729, 154427, 2011773);
+update DDMTemplateVersion set userid = 10159 where userid in (10729, 154427, 2011773);
