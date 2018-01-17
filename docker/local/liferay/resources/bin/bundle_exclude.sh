28866720665732edbd019123f3a9185701ca142f
allen-ziegenfus <allen.ziegenfus@liferay.com>
2018-01-17 14:32:55 +0100

    Make bundle exclusions less specific

--- docker/local/liferay/resources/bin/bundle_exclude.sh
+++ docker/local/liferay/resources/bin/bundle_exclude.sh
@@ -6,11 +6,11 @@ remove_module () {
     zip -d "${OSGI_FOLDER}/$1" $2
 }
 
-remove_module Liferay\ Connected\ Services\ Client.lpkg lcs-portlet-7.0.10.38.war
-remove_module Liferay\ Foundation.lpkg  com.liferay.portal.security.ldap-2.2.6.jar
-remove_module Liferay\ Foundation.lpkg  com.liferay.portal.settings.authentication.ldap.web-1.0.14.jar
-remove_module Liferay\ Documentum\ Connector.lpkg  documentum-hook-7.0.10.11.war
-remove_module Liferay\ Sharepoint\ Connector.lpkg  sharepoint-hook-7.0.10.4.war 
+remove_module Liferay\ Connected\ Services\ Client.lpkg lcs-portlet*
+remove_module Liferay\ Foundation.lpkg  com.liferay.portal.security.ldap*
+remove_module Liferay\ Foundation.lpkg  com.liferay.portal.settings.authentication.ldap*
+remove_module Liferay\ Documentum\ Connector.lpkg  documentum-hook*
+remove_module Liferay\ Sharepoint\ Connector.lpkg  sharepoint-hook*
 remove_module Liferay\ Marketplace.lpkg com.liferay.marketplace*
 remove_module Liferay\ Sync\ Connector.lpkg com.liferay.sync*
 remove_module Liferay\ Collaboration.lpkg com.liferay.message.boards*
