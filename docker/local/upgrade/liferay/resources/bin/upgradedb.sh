918ddd7ae03ab938eb96b4f4f35e11b0a5b874fd
allen-ziegenfus <allen.ziegenfus@liferay.com>
2018-01-19 16:05:33 +0100

    Changes to db upgrade, include log

--- docker/local/upgrade/liferay/resources/bin/upgradedb.sh
+++ docker/local/upgrade/liferay/resources/bin/upgradedb.sh
@@ -9,3 +9,11 @@ cd ${DIR}
 export JAVA_OPTS="-Xms4096m -Xmx4096m -XX:MaxNewSize=2048m -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled"
 java -jar "${DIR}/com.liferay.portal.tools.db.upgrade.client.jar" -j "${JAVA_OPTS}"
 
+echo "grepping for errors"
+grep -i error ${DIR}/upgrade.log
+echo "grepping for exceptions"
+grep -i exception ${DIR}/upgrade.log
+echo "grepping for warnings"
+grep -i WARN ${DIR}/upgrade.log
+
+cp ${DIR}/upgrade.log /tmp/liferay/
