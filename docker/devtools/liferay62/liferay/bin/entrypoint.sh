952355cf545fda06e4841cd170a7dfae16de26cd
ryanschuhler <ryan.schuhler@liferay.com>
2018-01-30 13:01:55 -0800

    Fixes for 62 docker container

--- docker/devtools/liferay62/liferay/bin/entrypoint.sh
+++ docker/devtools/liferay62/liferay/bin/entrypoint.sh
@@ -2,12 +2,12 @@
 
 cp -rv /tmp/liferay/* ${LIFERAY_HOME}
 
-for script in ${LIFERAY_HOME}/bin/*.sh; do 
+for script in ${LIFERAY_HOME}/bin/*.sh; do
     if [ -f ${script} ]; then
         bash -x "${script}"
-    fi 
+    fi
 done
 
-bash /usr/local/bin/wait-for-it.sh -t 600 mariadb:3306
+bash /usr/local/bin/wait-for-it.sh -t 600 mariadb62:3306
 
 exec "$@"
