cad668f58eb63f7c99b3cc8f4cf9abb576116739
ryanschuhler <ryan.schuhler@liferay.com>
2018-01-29 16:42:50 -0800

    Updating liferay dockerfile

--- docker/liferay/build-resources/apply-fix-packs.sh
+++ docker/liferay/build-resources/apply-fix-packs.sh
@@ -8,37 +8,37 @@ PATCH_FILES=`find $PATCH_FILES_DIR -type f`
 
 # Exit if no fixpack urls defined
 if [ -z "$LIFERAY_FIXPACK_DOWNLOAD_URLS" ] && [ -z "$PATCH_FILES" ]
-then 
+then
     exit 0
 fi
 
 PATCHING_TOOL_HOME=/opt/java/liferay/patching-tool
 
 # Delete any patches that may have been delivered with the bundle
-rm -fv ${PATCHING_TOOL_HOME}/patches/*.zip 
+rm -fv ${PATCHING_TOOL_HOME}/patches/*.zip
 
-cd ${PATCHING_TOOL_HOME}/patches/ 
+cd ${PATCHING_TOOL_HOME}/patches/
 
 if [ ! -z "$PATCH_FILES" ]
 then
     for PATCH_FILE in ${PATCH_FILES}
-    do 
+    do
         mv -v $PATCH_FILE ${PATCHING_TOOL_HOME}/patches/
     done
 else
     # Parse semi colon delimited list of URLs into an array
-    IFS=';' read -r -a URLARRAY <<< "$LIFERAY_FIXPACK_DOWNLOAD_URLS" 
+    IFS=';' read -r -a URLARRAY <<< "$LIFERAY_FIXPACK_DOWNLOAD_URLS"
 
     # Iterate through uRLs and download patches / hot fixes
-    for URL in "${URLARRAY[@]}" 
-    do 
-        curl -fSL -O "$URL" 
+    for URL in "${URLARRAY[@]}"
+    do
+        curl -fSL -O "$URL"
         rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-    done 
-fi 
+    done
+fi
 
-sed -i 's/tomcat[^\/]*/tomcat/g' ${PATCHING_TOOL_HOME}/default.properties 
+sed -i 's/tomcat[^\/]*/tomcat/g' ${PATCHING_TOOL_HOME}/default.properties
 
-bash ${PATCHING_TOOL_HOME}/patching-tool.sh install -force 
+bash ${PATCHING_TOOL_HOME}/patching-tool.sh install -force
 rm -rfv ${PATCHING_TOOL_HOME}/patches/*
 rm -rfv /opt/java/liferay/tomcat/webapps/ROOT/WEB-INF/patching-backup.zip
