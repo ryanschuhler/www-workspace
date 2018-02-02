14d3e34fba41a9b2ac9dc6a73bdbb8dbe1cc6a85
allen-ziegenfus <allen.ziegenfus@liferay.com>
2018-02-02 17:14:57 +0100

    Get hubspot working again in 62 context

--- docker/devtools/liferay62/setup-env.sh
+++ docker/devtools/liferay62/setup-env.sh
@@ -5,9 +5,11 @@ SOURCE_BUNDLE_FILE=liferay-portal-tomcat-6.2-ee-sp20-20170717160924965.zip
 PATCH_FILES=liferay-fix-pack-portal-164-6210.zip
 DATABASE=www_lportal-2018-02-01_19-00-PST.sql.gz
 
-# need set this more dynamically so it works across computer setups
-DOCKER_BASE=~/liferay/www-workspace/docker/devtools/liferay62/liferay
-MARIADB=~/liferay/www-workspace/docker/devtools/liferay62/mariadb
+REPO_ROOT=~/liferay/www-workspace 
+
+DOCKER_BASE=${REPO_ROOT}/docker/devtools/liferay62/liferay
+MARIADB=${REPO_ROOT}/docker/devtools/liferay62/mariadb
+DOCKER_HUBSPOT=${REPO_ROOT}/docker/devtools/hubspot 
 
 BUNDLE_FILE=liferay-bundle.zip
 
@@ -25,6 +27,7 @@ CACHE=~/.liferay/
 cp -v ${CACHE}/licenses/${LICENSE_FILE} ${DOCKER_BASE}/resources/image-deploy
 cp -v ${CACHE}/bundles/${SOURCE_BUNDLE_FILE} ${DOCKER_BASE}/${BUNDLE_FILE}
 cp -v ${CACHE}/databases/${DATABASE} ${MARIADB}/resources/docker-entrypoint-initdb.d/
+cp -v ${CACHE}/hubspot/hubspot.json ${DOCKER_HUBSPOT}/resources
 
 for PATCH_FILE in ${PATCH_FILES}
 do
