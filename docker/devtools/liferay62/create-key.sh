f98802de0f08d91637adc410bde1eea35d69acad
protoEvangelion <ryantgarant@gmail.com>
2018-04-02 14:25:38 -0700

    Adjust Catalina opts and ssh script

--- docker/devtools/liferay62/create-key.sh
+++ docker/devtools/liferay62/create-key.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 OUTPUTDIR=liferay/ssh
 mkdir -p ${OUTPUTDIR}
-ssh-keygen -t rsa -b 4096 -C "${EMAIL}" -f ${OUTPUTDIR}/id_rsa -N ''
+ssh-keygen -t rsa -b 4096 -C "$(git config --global user.email)" -f ${OUTPUTDIR}/id_rsa -N ''
 
 curl -u "$(git config --global user.name)" \
   --data "{\"title\":\"`date +%m/%d/%Y-%H:%M:%S`_DOCKER-LIFERAY-6.2\",\"key\":\"`cat ${OUTPUTDIR}/id_rsa.pub`\"}" \
