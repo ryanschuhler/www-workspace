3e2564e6c40a4c0bdfeff49989e188d96e5cfdc5
ryanschuhler <ryan.schuhler@liferay.com>
2018-04-03 14:57:07 -0700

    Update files

--- docker/devtools/liferay62/file-vars.sh
+++ docker/devtools/liferay62/file-vars.sh
@@ -10,6 +10,6 @@ PATCHES_DIR=.liferay/patches
 # FILES
 LICENSE_FILE=activation-key-development-6.2ee-liferaycom.xml
 SOURCE_BUNDLE_FILE=liferay-portal-tomcat-6.2-ee-sp20-20170717160924965.zip
-PATCH_FILES=liferay-fix-pack-portal-164-6210.zip
-DATABASE=www_lportal-2018-03-26_19-00-PDT.sql.gz
+PATCH_FILES=liferay-fix-pack-portal-165-6210.zip
+DATABASE=www_lportal-2018-04-02_19-00-PDT.sql.gz
 
