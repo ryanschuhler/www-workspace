dc9e007ad42b88e7b1c3c690755a53d29b30c2fa
allen-ziegenfus <allen.ziegenfus@liferay.com>
2018-07-13 12:22:53 -0500

    copy liferay upgrade config for 7.1

---
+++ docker/devtools/upgrade/liferay_71/resources/bin/upgradedb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+/opt/java/liferay/bin/wait-for-it.sh -t 600 upgrade_mariadb:3306
+
+DIR=/opt/java/liferay/tools/portal-tools-db-upgrade-client
+
+cp -rv /mnt/upgradetools/* /opt/java/liferay/tools
+
+cd ${DIR}
+export JAVA_OPTS="-Xms4096m -Xmx4096m -XX:MaxNewSize=2048m -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled"
+java -jar "${DIR}/com.liferay.portal.tools.db.upgrade.client.jar" -j "${JAVA_OPTS}"
+
+echo "grepping for errors"
+grep -i error ${DIR}/upgrade.log
+echo "grepping for exceptions"
+grep -i exception ${DIR}/upgrade.log
+echo "grepping for warnings"
+grep -i WARN ${DIR}/upgrade.log
+
+cp ${DIR}/upgrade.log /tmp/liferay/
