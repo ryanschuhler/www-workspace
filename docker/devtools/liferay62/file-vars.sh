2cff485ffa532b1bea9f3685e16d160dcfccbcc2
allen-ziegenfus <allen.ziegenfus@liferay.com>
2018-07-13 11:48:45 -0500

    Update db / hotfix

--- docker/devtools/liferay62/file-vars.sh
+++ docker/devtools/liferay62/file-vars.sh
@@ -11,6 +11,5 @@ PATCHES_DIR=~/.liferay/patches
 LICENSE_FILE=activation-key-development-6.2ee-liferaycom.xml
 SOURCE_BUNDLE_FILE=liferay-portal-tomcat-6.2-ee-sp20-20170717160924965.zip
 #PATCH_FILES=portal/liferay-fix-pack-portal-165-6210.zip
-PATCH_FILES=hotfix/liferay-hotfix-26906-6210.zip
-DATABASE=www_lportal-2018-05-07_19-00-PDT.sql.gz
-
+PATCH_FILES=hotfix/liferay-hotfix-26946-6210.zip
+DATABASE=www_lportal-2018-07-10_19-00-PDT.sql.gz
